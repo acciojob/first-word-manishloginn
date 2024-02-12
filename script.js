d2ea1ae243d0f3f5e27d0bd2a7d5b6a0e0bbdee8
manishloginn <132683386+manishloginn@users.noreply.github.com>
2024-02-12 10:49:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 function firstWord(s) {
   // your code here
+ 
+  if (s === null) {
+    return s;
+  }
+ 
+  const arr = [];
+  arr.push(s.charAt(0));
+ 
+  for (let i = 1; i < s.length; i++) {
+    if (s.charAt(i) === " ") {
+      return arr.join("").trim();
+    }
+    arr.push(s.charAt(i));
+  }
+ 
+  return arr.join("").trim();
 }
-
+ 
 // Do not change the code below
-
+ 
 const s = prompt("Enter String:");
 alert(firstWord(s));
